a3c1ad4ed8dda51b057ce2df9d8aad2749316e27
Joshua Martinez <8892161+joshermar@users.noreply.github.com>
2020-11-14 09:16:53 -0800

    completed locks

--- kern/include/synch.h
+++ kern/include/synch.h
@@ -75,8 +75,12 @@ void V(struct semaphore *);
 struct lock {
         char *lk_name;
         HANGMAN_LOCKABLE(lk_hangman);   /* Deadlock detector hook. */
-        // add what you need here
-        // (don't forget to mark things volatile as needed)
+        /* MY SOLUTION */
+        struct wchan *lk_wchan;
+        struct thread *volatile lk_holder;
+        struct spinlock lk_splk;
+        /* END MY SOLUTION*/
+
 };
 
 struct lock *lock_create(const char *name);
